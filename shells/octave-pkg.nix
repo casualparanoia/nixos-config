feb679817f08e980b27e09522b5867f2c5cdef01
Yusuf Ali <nixosgit.relation593@passmail.net>
2026-08-17 23:46:45 +0300

    Octave DevShell update

--- shells/octave-pkg.nix
+++ shells/octave-pkg.nix
@@ -3,14 +3,44 @@
 pkgsUnstable.mkShell {
   packages = [
     pkgsUnstable.octaveFull
+
+    # Build tools
     pkgsUnstable.gfortran
     pkgsUnstable.pkg-config
+
+    # Archive helpers
     pkgsUnstable.unzip
     pkgsUnstable.zip
   ];
 
   buildInputs = [
+    # Core numerical libraries
     pkgsUnstable.blas
     pkgsUnstable.lapack
+
+    # communications
+    pkgsUnstable.hdf5
+
+    # general
+    pkgsUnstable.nettle
+
+    # sqlite
+    pkgsUnstable.sqlite
+
+    # strings
+    pkgsUnstable.pcre2
+
+    # audio
+    pkgsUnstable.rtmidi
+    pkgsUnstable.alsa-lib
+    pkgsUnstable.jack2
+
+    # ltfat
+    pkgsUnstable.fftw
+    pkgsUnstable.fftwSinglePrec
+    pkgsUnstable.fftwFloat
+    pkgsUnstable.fftwLongDouble
+    pkgsUnstable.portaudio
+    pkgsUnstable.jdk
   ];
 }
